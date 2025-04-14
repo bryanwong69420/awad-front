a5fc4e14c1e6656882d1c8a826fbdad2d9525ef1
Wong Jia Cin <bryanwongjiacin@gmail.com>
2025-04-14 14:54:28 +0800

    Implemented Model to Model Relationship

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -10,5 +10,11 @@ class ProductType extends Model
     use HasFactory;
 
     protected $table = 'product_type';
-
+    /**
+     * Get all products of this product type
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'product_type', 'id');
+    }
 }
